90ea3011d5734dea5d0b962b1f92e3673b90d1cf
gdar463 <gdar463@gmail.com>
2025-09-22 18:33:02 +0200

    finished swap 09-22

--- Pointers_09-22/swap/main.cpp
+++ Pointers_09-22/swap/main.cpp
@@ -1,10 +1,33 @@
+#include <iostream>
+
 // Per gestire meglio i progetti uso CMake e un'unico main file
 // per i vari main li divido attraverso namespace
 #ifdef USING_CMAKE
 namespace Swap {
 #endif
 
-int main() { return 1; }
+void swapValues(int *ptr1, int *ptr2) {
+  int temp = *ptr1;
+  *ptr1 = *ptr2;
+  *ptr2 = temp;
+}
+
+int main() {
+  int a{5}, b{10};
+  int *ptrA{&a}, *ptrB{&b};
+
+  std::cout << "Prima dello swap" << std::endl;
+  std::cout << "A: " << a << std::endl;
+  std::cout << "B: " << b << std::endl;
+
+  swapValues(ptrA, ptrB);
+
+  std::cout << "Dopo lo swap" << std::endl;
+  std::cout << "A: " << a << std::endl;
+  std::cout << "B: " << b << std::endl;
+
+  return 0;
+}
 
 #ifdef USING_CMAKE
 }
