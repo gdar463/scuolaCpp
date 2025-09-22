4744ea8b61b25e946ede2b8be7d1e8a2b3e1425e
gdar463 <gdar463@gmail.com>
2025-09-22 18:40:35 +0200

    finished print 09-22

--- Pointers_09-22/print/print.cpp
+++ Pointers_09-22/print/print.cpp
@@ -1,10 +1,25 @@
+#include <iostream>
+
 // Per gestire meglio i progetti uso CMake e un'unico main file
 // per i vari main li divido attraverso namespace
 #ifdef USING_CMAKE
 namespace Print {
 #endif
 
-int main() { return 1;}
+void printArray(int *array, int size) {
+  for (int i = 0; i < size; i++) {
+    std::cout << "Elemento n° " << i << ": " << *(array + i) << std::endl;
+  }
+}
+
+int main() {
+  int myArray[5]{1, 2, 3, 4, 5};
+  // Possiamo passare direttamente `myArray` alla funzione
+  // perché `myArray` in realtà è un puntatore che punta
+  // al primo elemento dell'array
+  printArray(myArray, 5);
+  return 0;
+}
 
 #ifdef USING_CMAKE
 }
