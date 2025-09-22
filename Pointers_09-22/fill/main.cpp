e930d25569425f2a7936993ddf5b910e5c917a03
gdar463 <gdar463@gmail.com>
2025-09-22 18:28:48 +0200

    finished fill 09-22

--- Pointers_09-22/fill/main.cpp
+++ Pointers_09-22/fill/main.cpp
@@ -1,10 +1,30 @@
+#include <iostream>
+
 // Per gestire meglio i progetti uso CMake e un'unico main file
 // per i vari main li divido attraverso namespace
 #ifdef USING_CMAKE
 namespace Fill {
 #endif
 
-int main() { return 1; }
+int *createAndFillArray(int size) {
+  int *array{new int[size]};
+  for (int i = 0; i < size; i++) {
+    array[i] = i;
+  }
+  return array;
+}
+
+int main() {
+  int size;
+  std::cout << "Inserisci la dimensione dell'array: ";
+  std::cin >> size;
+  int *array{createAndFillArray(size)};
+  for (int i = 0; i < size; i++) {
+    std::cout << "Elemento n° " << i << ": " << array[i] << std::endl;
+  }
+  delete[] array;
+  return 0;
+}
 
 #ifdef USING_CMAKE
 }
