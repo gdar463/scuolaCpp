930389aedac5b65f4924df21b7a6c5883d106ded
gdar463 <gdar463@gmail.com>
2025-09-23 12:53:00 +0200

    setup pointers 09-23

--- Pointers_09-23/main.cpp
+++ Pointers_09-23/main.cpp
@@ -1,1 +1,13 @@
+#include <cstddef>
+
+size_t cstr_len(const char *s) { return 0; };
+
+bool cstr_copy(char *dst, size_t cap, const char *src) { return true; };
+
+bool cstr_cat(char *dst, size_t cap, const char *src) { return true; }
+
+int cstr_cmp(const char *a, const char *b) { return 0; };
+
+void cstr_remove_char(char *s, char ch) {};
+
 int main() { return 0; }
