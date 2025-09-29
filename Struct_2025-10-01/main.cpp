f848d8447d92b33904ac8ef3e376b81686e59b21
gdar463 <gdar463@gmail.com>
2025-09-29 15:53:52 +0200

    started struct_2025-10-01

---
+++ Struct_2025-10-01/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello from Struct_2025-10-01!" << std::endl;
+
+  return 0;
+}
---
+++ scripts/struct_2025-10-01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cmake --build build -t Struct_2025-10-01
+if [ $? -eq 0 ]; then
+    mv build/Struct_2025-10-01 build/struct_2025-10-01
+    if [[ "${PWD}" != "/home/dario/projects/cpp/scuola/Struct_2025-10-01/" ]]; then
+        cd Struct_2025-10-01
+    fi
+    echo
+    ../build/struct_2025-10-01 "${@:1}" 2>&1
+fi
